11856253ea0aba362dd650b4faef4880d898db74
Shamini <162007196+shaminiv02@users.noreply.github.com>
2026-07-23 10:50:12 +0530

    Time: 21 ms (57.22%) | Memory: 15.8 MB (97.45%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+
+            if key in groups:
+                groups[key].append(word)
+            else:
+                groups[key] = [word]
+
+        return list(groups.values())
