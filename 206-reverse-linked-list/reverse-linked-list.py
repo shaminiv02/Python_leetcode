238364710d23b9c46983febdac626b41f09367c3
Shamini <162007196+shaminiv02@users.noreply.github.com>
2026-07-24 08:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (30.39%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+
+        while head:
+            head.next, prev, head = prev, head, head.next
+
+        return prev
+        
